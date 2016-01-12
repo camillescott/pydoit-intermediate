5c39c155a7af354230191298c18021c4f8176f0c
Camille Scott <camille.scott.w@gmail.com>
2016-01-11 17:44:30 -0800

    Add dodo.py

--- dodo.py
+++ dodo.py
@@ -1,3 +1,30 @@
+from doit.tools import run_once
+
 def task_download_data():
-    return {'actions': ['curl -L http://tinyurl.com/zdhpjlk > Melee_data.csv'],
-            'targets': ['Melee_data.csv']}
+     return {'actions': ['curl -OL https://s3.amazonaws.com/pydoit-intermediate/Melee_data.csv.gz'],
+             'targets': ['Melee_data.csv.gz'],
+             'uptodate': [run_once]}
+
+def task_gunzip_data():
+    return {'actions': ['gunzip -c %(dependencies)s > %(targets)s'],
+            'targets': ['Melee_data.csv'],
+            'file_dep': ['Melee_data.csv.gz']}
+
+def task_plot_heatmap():
+
+    def do_plot(dependencies, targets):
+        import matplotlib.pyplot as plt
+        import pandas as pd
+        import seaborn as sns
+
+        data = pd.read_csv(list(dependencies)[0], index_col=0)
+        clst = sns.clustermap(data, linewidths=.5, figsize=(8, 8), square=True,
+                              method='ward', z_score=0, row_cluster=False)
+        # I'm a stickler, so rotate the labels nicely
+        plt.setp(clst.ax_heatmap.yaxis.get_majorticklabels(), rotation=0)
+        plt.setp(clst.ax_heatmap.xaxis.get_majorticklabels(), rotation=90)
+        clst.savefig(targets[0])
+
+    return {'actions': [do_plot],
+            'file_dep': ['Melee_data.csv'],
+            'targets': ['Melee_data.csv.heatmap.pdf']}
