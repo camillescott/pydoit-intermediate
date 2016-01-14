c1e8c8fb5ed546d8600063feda49b4a26fba6ba5
Camille Scott <camille.scott.w@gmail.com>
2016-01-13 16:36:27 -0800

    Update dodo to fix syntax error

--- dodo.py
+++ dodo.py
@@ -12,11 +12,13 @@ def task_publish():
 
     cmd_list = ['touch _build/html/.nojekyll',
                 'git add _build/',
-                'git commit  -m "Generated gh-pages for `git log master -1 --pretty=short --abbrev-commit`',
+                'git commit  -m "Generated gh-pages for `git log master -1 '\
+                '--pretty=short --abbrev-commit`"',
                 'git subtree split --prefix _build/html -b gh-pages',
                 'git push -f origin gh-pags:gh-pages',
                 'git branch -D gh-pages']
 
     return {'task_dep': ['build_html'],
+            'file_dep': input_files,
             'actions': cmd_list}
 
